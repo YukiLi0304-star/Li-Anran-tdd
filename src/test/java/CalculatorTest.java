621a8fd825dc5d2aae7d4088656da629a5b7d6f6
YukiLi0304-star <YukiLi0304@outlook.com>
2025-10-13 23:52:29 +0800

    Test1

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,8 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
     @Test
-    void testAdd() {
-        
+    void testAdd() {      
         Calculator c = new Calculator();
         assertEquals(5, c.add(2, 3));
     }
