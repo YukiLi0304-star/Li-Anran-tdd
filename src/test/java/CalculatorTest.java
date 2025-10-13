3b742916a2cb18079a4acae2646bc1933ef371e6
YukiLi0304-star <YukiLi0304@outlook.com>
2025-10-13 23:28:28 +0800

    First

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @Test
+    void testAdd() {
+        Calculator c = new Calculator();
+        assertEquals(5, c.add(2, 3));
+    }
+}
